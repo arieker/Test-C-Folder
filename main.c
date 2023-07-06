4a37fca3df8d714a745d2a71685fc6173aba4496
ANTHONY <riekeranthony@gmail.com>
2023-07-06 18:43:57 -0400

    Merge & Quick sort

--- main.c
+++ main.c
@@ -77,9 +77,61 @@ void printArray(int A[], int size)
 	printf("\n");
 }
 
+// Function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+  
+// Partition the array using the last element as the pivot
+int partition(int arr[], int low, int high)
+{
+    // Choosing the pivot
+    int pivot = arr[high];
+  
+    // Index of smaller element and indicates
+    // the right position of pivot found so far
+    int i = (low - 1);
+  
+    for (int j = low; j <= high - 1; j++) {
+  
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+  
+            // Increment index of smaller element
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+  
+// The main function that implements QuickSort
+// arr[] --> Array to be sorted,
+// low --> Starting index,
+// high --> Ending index
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+  
+        // pi is partitioning index, arr[p]
+        // is now at right place
+        int pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
 // Driver code
 int main()
 {
+    // Merge sort
 	int arr[] = { 12, 11, 13, 5, 6, 7 };
 	int arr_size = sizeof(arr) / sizeof(arr[0]);
 
@@ -90,5 +142,21 @@ int main()
 
 	printf("\nSorted array is \n");
 	printArray(arr, arr_size);
-	return 0;
+	
+    printf("---------------------\n");
+
+    // Quick sort
+    int arr2[] = { 10, 7, 8, 9, 1, 5 };
+    int N = sizeof(arr2) / sizeof(arr2[0]);
+
+    printf("Given array is \n");
+    printArray(arr2, N);
+  
+    // Function call
+    quickSort(arr2, 0, N - 1);
+    printf("\nSorted array is \n");
+    for (int i = 0; i < N; i++)
+        printf("%d ", arr2[i]);
+    
+    return 0;
 }
